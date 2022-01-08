181cdacbd55e9e49cf14da41c727e4844073cdd0
inoccent khan <k.ashar10@outlook.com>
2022-01-09 00:29:18 +0500

    Add project files.

---
+++ html_Helper/Controllers/HelperController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html_Helper.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+        public ActionResult Standardd()
+        {
+            return View();
+        }
+    }
+}
