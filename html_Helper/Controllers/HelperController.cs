7ae01ced4639a010cda002d3d75a00d8730b6dae
inoccent khan <k.ashar10@outlook.com>
2022-01-09 00:29:18 +0500

    Strongly HTML helper completed!Add project files.

---
+++ html_Helper/Controllers/HelperController.cs
@@ -0,0 +1,42 @@
+﻿using html_Helper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html_Helper.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+        public ActionResult Standardd()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Stgstd()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Stgstd(Employee emp)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ html_Helper/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace html_Helper.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Gender { get; set; }
+
+        public bool City { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
